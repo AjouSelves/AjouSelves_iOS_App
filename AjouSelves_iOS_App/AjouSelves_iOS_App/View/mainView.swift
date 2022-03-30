efab61c8ae4412b2d2143fcf4a9a58c1b6a29fe3
Jayfunf <simh3077@gmail.com>
2022-03-30 19:58:14 +0900

    change: disable navigationTitle -> ToolbarItem_Button update: fix test list size 10 -> 30

--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/mainView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/mainView.swift
@@ -11,27 +11,51 @@ struct mainView: View {
     var body: some View {
         //펀딩 목록 불러오기
         NavigationView{
-            
             //test List
-            List(1...10, id: \.self) { index in
+            List(1...30, id: \.self) { index in
                 NavigationLink(
                     destination: Text("아이템 \(index) 번 의 세부사항"),
                         label: {
                             Text("아이템 \(index)")
-                    })
+                        })
             }
             //test code exit
             
-            .navigationTitle("펀딩 목록")
+            //.navigationTitle("펀딩 목록")
+            .navigationBarTitleDisplayMode(.automatic)
             .toolbar{
-                ToolbarItem(placement: .navigationBarTrailing){
+                ToolbarItemGroup(placement: .navigationBarTrailing){
                     Button(action: {
                         print("Clicked")
                     }, label: {
-                        Image(systemName: "person")
+                        Image(systemName: "magnifyingglass")
+                    })
+                    Button(action: {
+                        print("Clicked2")
+                    }, label: {
+                        Image(systemName: "line.3.horizontal.circle")
+                    })
+                    Button(action: {
+                        print("Clicked3")
+                    }, label: {
+                        Image(systemName: "bell")
+                    })
+                }
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button(action: {
+                        print("Clicked4")
+                    }, label: {
+                        Text("아주대")
+                            .font(.title2)
+                            .bold()
+                            .foregroundColor(Color.black)
+                        
                     })
                 }
             }
+            .frame(height: 700)
+            // .border(Color.blue) // Title밑 Tabview밑의 파란 줄
+            .padding(-15)
         }
     }
 }
