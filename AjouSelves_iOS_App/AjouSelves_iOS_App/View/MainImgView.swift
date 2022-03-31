dd748742958415480fd364fb0a7229f327f525df
Jayfunf <simh3077@gmail.com>
2022-03-31 21:44:43 +0900

    add: MainImgView.swift

---
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/MainImgView.swift
@@ -0,0 +1,21 @@
+//
+//  MainImgView.swift
+//  AjouSelves_iOS_App
+//
+//  Created by Minhyun Cho on 2022/03/31.
+//
+
+import Foundation
+import SwiftUI
+
+struct MainImgView: View {
+    var body: some View {
+        Circle()
+    }
+}
+
+struct MainImgView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainImgView()
+    }
+}
