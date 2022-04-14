ee0a1fbafae344cf329e4a9bd4bbe80f57d3fd96
Jayfunf <simh3077@gmail.com>
2022-04-14 16:49:11 +0900

    add some View: buyingViewModel

---
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/ViewModel/buyingViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  buyingViewModel.swift
+//  AjouSelves_iOS_App
+//
+//  Created by Minhyun Cho on 2022/04/13.
+//
+
+import Foundation
+import Combine
+
+class buyingViewModel {
+    
+    @Published var title: String = ""
+    @Published var price: String = ""
+    @Published var text: String = ""
+    
+    
+    
+}
