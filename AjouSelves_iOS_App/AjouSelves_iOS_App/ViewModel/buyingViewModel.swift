1c0fee1e0e6815a3625307de0d8b10699f733440
Jayfunf <simh3077@gmail.com>
2022-04-24 10:05:19 +0900

    add: Alamofire, send()

--- AjouSelves_iOS_App/AjouSelves_iOS_App/ViewModel/buyingViewModel.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/ViewModel/buyingViewModel.swift
@@ -7,13 +7,26 @@
 
 import Foundation
 import Combine
+import Alamofire
 
-class buyingViewModel {
+class buyingViewModel: ObservableObject {
+    
+    var projAddUrl = "http://44.202.49.100:3000/proj/add"
     
     @Published var title: String = ""
-    @Published var price: String = ""
-    @Published var text: String = ""
+    @Published var explained: String = ""
+    @Published var min_num: Int = 0
+    @Published var category: String = ""
+    @Published var required: [String] = [""]
     
-    
-    
+    func send() {
+        let param: Parameters = ["userid": 3, "title" : title, "explained" : explained, "min_num" : min_num, "category" : category, "required" : ["asd" : "asd", "fgh": "fgh"]] //dummyData in required
+        print("buyingViewModel.send() method : \(title), \(explained), \(min_num), \(category), \(required)")
+        print(param)
+        AF.request(projAddUrl, method: .post, parameters: param, encoding: JSONEncoding.default)
+            .responseJSON(){ json in
+                print(json)
+                print(param)
+            }
+    }
 }
