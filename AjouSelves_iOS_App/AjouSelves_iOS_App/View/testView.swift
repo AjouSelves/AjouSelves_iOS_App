e87f92e83dfc4c217c0f58f909b94422ccf4111d
Jayfunf <simh3077@gmail.com>
2022-05-04 14:14:54 +0900

    Code refactoring

--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/testView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/testView.swift
@@ -31,27 +31,6 @@ struct testView: View {
                         Label("설정", systemImage: "gear")
                     }
             }
-//            VStack{
-//                Spacer()
-//                HStack{
-//                    Spacer()
-//                    Button(action: {
-//                        BuyingView()
-//                        print("Testing Plus Button")
-//                    }, label: {
-//                        Text("+")
-//                            .font(.system(.largeTitle))
-//                            .frame(width: 77, height: 70)
-//                            .foregroundColor(Color.white)
-//                            .padding(.bottom, 7)
-//                    })
-//                    .background(Color.blue)
-//                    .cornerRadius(38.5)
-//                    .padding()
-//                    .shadow(color: Color.black.opacity(0.3), radius: 3, x: 3, y: 3)
-//
-//                }
-//            }
         }
     }
 }
