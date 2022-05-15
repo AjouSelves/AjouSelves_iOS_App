c2a37e6ba4e0b9dd6523bb8039b4b5df4def2c50
Jayfunf <simh3077@gmail.com>
2022-05-16 01:01:53 +0900

    add: startView struct for changing multiple view's

---
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/startView.swift
@@ -0,0 +1,28 @@
+//
+//  startView.swift
+//  AjouSelves_iOS_App
+//
+//  Created by Minhyun Cho on 2022/05/13.
+//
+
+import SwiftUI
+
+struct startView: View {
+    
+    @ObservedObject var viewrouter: viewRouter
+    
+    var body: some View {
+        if viewrouter.currentPage == "ContentView"{
+            ContentView(productdataVM: productDataViewModel(), viewrouter: viewrouter)
+        }
+        else if viewrouter.currentPage == "testView"{
+            testView(viewrouter: viewrouter)
+        }
+    }
+}
+
+struct startView_Previews: PreviewProvider {
+    static var previews: some View {
+        startView(viewrouter: viewRouter())
+    }
+}
