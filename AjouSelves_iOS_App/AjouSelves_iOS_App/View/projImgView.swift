82b6518212db617a948e069b0fa51fa37ffb9eb1
Jayfunf <simh3077@gmail.com>
2022-05-16 17:36:46 +0900

    add: projImgView

---
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/projImgView.swift
@@ -0,0 +1,20 @@
+//
+//  projImgView.swift
+//  AjouSelves_iOS_App
+//
+//  Created by Minhyun Cho on 2022/05/16.
+//
+
+import SwiftUI
+
+struct projImgView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct projImgView_Previews: PreviewProvider {
+    static var previews: some View {
+        projImgView()
+    }
+}
