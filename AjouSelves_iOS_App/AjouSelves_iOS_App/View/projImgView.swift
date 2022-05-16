b38f8f8b03bf940d61cc224095d85ac445e4d61a
Jayfunf <simh3077@gmail.com>
2022-05-16 18:28:06 +0900

    import: URLImage add: URLImage function

--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/projImgView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/projImgView.swift
@@ -5,16 +5,29 @@
 //  Created by Minhyun Cho on 2022/05/16.
 //
 
+import Foundation
 import SwiftUI
+import URLImage
 
 struct projImgView: View {
+    var imageUrl: URL
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        URLImage(imageUrl) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }
+        .frame(width: 400, height: 400)
     }
 }
 
 struct projImgView_Previews: PreviewProvider {
     static var previews: some View {
-        projImgView()
+        
+        let url = URL(string: "https://randomuser.me/api/portraits/women/21.jpg")!
+        
+        projImgView(imageUrl: url)
     }
 }
