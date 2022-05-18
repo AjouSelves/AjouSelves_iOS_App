b902fe7b85158471c0272c116a14f20b5a283f18
Jayfunf <simh3077@gmail.com>
2022-05-18 18:44:02 +0900

    add: projAddSingleUrl, projAddMultiUrl for upload images to server

--- AjouSelves_iOS_App/AjouSelves_iOS_App/ViewModel/sellingViewModel.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/ViewModel/sellingViewModel.swift
@@ -12,6 +12,8 @@ import Alamofire
 class sellingViewModel: ObservableObject {
     
     var projAddUrl = "http://52.206.105.200:3000/proj/add"
+    var projAddSingleUrl = "http://52.206.105.200:3000/proj/add/single"
+    var projAddMultiUrl = "http://52.206.105.200:3000/proj/add/multi"
     
     @Published var title: String = ""
     @Published var explained: String = ""
@@ -22,6 +24,42 @@ class sellingViewModel: ObservableObject {
     var productdataViewModels = productDataViewModel()
 
     func send() {
+        let tokenHeader: HTTPHeaders = [
+            "Authorization": "\(productdataViewModels.userToken)",
+                    "Accept": "application/json",
+                    "Content-Type": "application/json" ]
+        
+        let param: Parameters = ["userid": 30, "title" : title, "explained" : explained, "min_num" : min_num, "category" : category, "required" : "test_required"] //dummyData in required
+        print("buyingViewModel.send() method : \(title), \(explained), \(min_num), \(category), \(required)")
+        AF.request(projAddUrl, method: .post, parameters: param, encoding: JSONEncoding.default, headers: tokenHeader)
+            .responseJSON(){ json in
+                print(json)
+                // 오류 메시지 변환
+//                if let data = json.data, let success = String(data: data, encoding: .utf8) {
+//                    print(success)
+//                }
+            }
+    }
+    
+    func single() {
+        let tokenHeader: HTTPHeaders = [
+            "Authorization": "\(productdataViewModels.userToken)",
+                    "Accept": "application/json",
+                    "Content-Type": "application/json" ]
+        
+        let param: Parameters = ["userid": 30, "title" : title, "explained" : explained, "min_num" : min_num, "category" : category, "required" : "test_required"] //dummyData in required
+        print("buyingViewModel.send() method : \(title), \(explained), \(min_num), \(category), \(required)")
+        AF.request(projAddUrl, method: .post, parameters: param, encoding: JSONEncoding.default, headers: tokenHeader)
+            .responseJSON(){ json in
+                print(json)
+                // 오류 메시지 변환
+//                if let data = json.data, let success = String(data: data, encoding: .utf8) {
+//                    print(success)
+//                }
+            }
+    }
+    
+    func multi() {
         let tokenHeader: HTTPHeaders = [
             "Authorization": "\(productdataViewModels.userToken)",
                     "Accept": "application/json",
