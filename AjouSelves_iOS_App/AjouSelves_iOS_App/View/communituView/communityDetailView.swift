adba5654179a86a3cf454c3782c29ddce61691a1
Minhyun Cho <simh3077@gmail.com>
2022-05-27 17:01:41 +0900

    add: communityDetailView

---
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/communituView/communityDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  communityDetailView.swift
+//  AjouSelves_iOS_App
+//
+//  Created by Minhyun Cho on 2022/05/27.
+//
+
+import SwiftUI
+
+struct communityDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct communityDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        communityDetailView()
+    }
+}
