65b12d6b6088d0a150a837e96c56a746f3f7b151
Minhyun Cho <simh3077@gmail.com>
2022-05-28 05:03:09 +0900

    modify: for community

--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/communituView/communityDetailView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/communituView/communityDetailView.swift
@@ -8,13 +8,52 @@
 import SwiftUI
 
 struct communityDetailView: View {
+    @ObservedObject var productDataVM = productDataViewModel()
+    
+    var prdData: postAllData
+    
+    init(_ prdData : postAllData) {
+        self.prdData = prdData
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            ScrollView{
+                Divider()
+                ScrollView(.horizontal) {
+                    projImgView(imageUrl: prdData.profileImgUrl)
+                }
+                Divider()
+                Spacer()
+                VStack(alignment: .leading){
+                    Text("\(prdData.description_nickname)")
+                        .font(.system(size: 20))
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.5)
+                        .foregroundColor(Color.gray)
+                    Text("\(prdData.description_created_at)")
+                        .font(.system(size: 40))
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.5)
+                    Text("\(prdData.description_title)")
+                        .font(.system(size: 40))
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.5)
+                }.frame(width: 350, height: 70, alignment: .leading).padding()
+                VStack(alignment: .leading){
+                    Text("\(prdData.description_explained)")
+                        .font(.system(size: 15))
+                        .minimumScaleFactor(0.5)
+                        .padding()
+                }
+            }
+        }
+        .setTabBarVisibility(isHidden: true) // 프로젝트 디테일 뷰로 들어가면 TabBar비활성화
     }
 }
 
 struct communityDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        communityDetailView()
+        communityDetailView(postAllData.getDummy())
     }
 }
--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/communituView/communityListView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/communituView/communityListView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct communityListView: View {
+    
+    var prdData : postAllData
+
+    init(_ prdData : postAllData) {
+        self.prdData = prdData
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            thumbnailView(imageUrl: prdData.profileImgUrl)
+                .frame(width: 130, height: 130)
+                .clipped()
+            VStack(alignment: .leading, spacing: 5){
+                Text("\(prdData.description_title)")
+                    .fontWeight(.heavy)
+                    .font(.system(size: 20))
+                    .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                    .minimumScaleFactor(0.5)
+                Text("\(prdData.description_explained)")
+                    //.fontWeight(.heavy)
+                    .font(.system(size: 25))
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.5)
+                HStack {
+                    Text("\(prdData.description_created_at)")
+                        //.fontWeight(.heavy)
+                        .font(.system(size: 15))
+                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                        .minimumScaleFactor(0.5)
+                    Text("\(prdData.description_nickname)")
+                        //.fontWeight(.heavy)
+                        .font(.system(size: 15))
+                        .lineLimit(/*@START_MENU_TOKEN@*/2/*@END_MENU_TOKEN@*/)
+                        .minimumScaleFactor(0.5)
+                }
+            }
+        }.frame(minWidth: 100, maxWidth: .infinity, minHeight: 0, maxHeight: 100, alignment: .leading).padding(.vertical)
     }
 }
 
 struct communityListView_Previews: PreviewProvider {
     static var previews: some View {
-        communityListView()
+        communityListView(postAllData.getDummy())
     }
 }
