f7185e0744f09dee7f4417f577f20d050d2403a5
Minhyun Cho <simh3077@gmail.com>
2022-05-31 05:03:15 +0900

    git init

--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/CommunityView/communityView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/CommunityView/communityView.swift
@@ -6,27 +6,62 @@
 //
 
 import SwiftUI
+import URLImage
 
 struct communityView: View {
     
     //MARK: 테스팅
     @ObservedObject var ProductDataViewModel = productDataViewModel()
     
+    init() {
+        ProductDataViewModel.refreshPostAll()
+    }
+    
     var body: some View {
         NavigationView {
-            List(ProductDataViewModel.projectAllDataParcings, id: \.self) { adata in
-                //ProjListView(adata)
-                //print(adata)
+            List(ProductDataViewModel.postAllDatas, id: \.self) { adata in
                 NavigationLink(
-                    destination: Text("Test"),
+                    destination: communityDetailView(adata),
                         label: {
-                            //Text("Test")
-                            ProjListView(adata)
+                            communityListView(adata)
                         })
+            }
+            .setTabBarVisibility(isHidden: false) // 다시 뷰로 돌아오면 TabBar활성화
+            // iOS 15부터 지원...
+            .refreshable {
+                //productDataViewModel.init()
+                ProductDataViewModel.refreshPostAll()
+            }.toolbar{
+                ToolbarItemGroup(placement: .navigationBarTrailing){
+                    NavigationLink(
+                        destination: Text("검색"),
+                            label: {
+                                Image(systemName: "magnifyingglass")
+                    })
+                    NavigationLink(
+                        destination: communityAddView(productdataVM: productDataViewModel()),
+                            label: {
+                                Image(systemName: "plus")
+                    })
+                    NavigationLink(
+                        destination: Text("아직 알림이 없군요🔔"),
+                            label: {
+                                Image(systemName: "bell")
+                    })
+                }
+                ToolbarItem(placement: .navigationBarLeading){
+                    Button(action: {
+                        print("Clicked4")
+                    }, label: {
+                        Image("로고_PNG2")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 170, height: 170, alignment: .leading)
+                    })
+                }
             }
             .padding(-15)
         }
-        
     }
 }
 
