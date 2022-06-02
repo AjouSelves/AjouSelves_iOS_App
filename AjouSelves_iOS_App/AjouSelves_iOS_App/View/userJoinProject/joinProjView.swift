3e91fb624aee1d58cce77e1a15227b6f5bf09804
Minhyun Cho <simh3077@gmail.com>
2022-06-02 17:40:38 +0900

    delete: NavigationView in body view

--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/userJoinProject/joinProjView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/userJoinProject/joinProjView.swift
@@ -15,25 +15,20 @@ struct joinProjView: View {
     }
     
     var body: some View {
-        NavigationView {
-            List(ProductDataViewModel.userJoinDetails, id: \.self) { adata in
-                NavigationLink(
-                    destination: userJoinProjDetailView(adata),
-                        label: {
-                            userJoinProjListView(adata)
-                        })
-            }
-            .setTabBarVisibility(isHidden: false) // 다시 뷰로 돌아오면 TabBar활성화
-            // iOS 15부터 지원...
-            .refreshable {
-                ProductDataViewModel.refreshJoinProj()
-//                ProductDataViewModel.refreshProj()
-            }
-            .padding(-15)
-            .navigationBarTitle("", displayMode: .inline)
-            .navigationBarHidden(true)
-            .navigationBarBackButtonHidden(true)
+        List(ProductDataViewModel.userJoinDetails, id: \.self) { adata in
+            NavigationLink(
+                destination: userJoinProjDetailView(adata),
+                label: {
+                    userJoinProjListView(adata)
+                })
         }
+        .setTabBarVisibility(isHidden: false) // 다시 뷰로 돌아오면 TabBar활성화
+        // iOS 15부터 지원...
+        .refreshable {
+            ProductDataViewModel.refreshJoinProj()
+        }
+        .padding(-15)
+        .navigationBarTitle("리스트", displayMode: .inline)
     }
 }
 
