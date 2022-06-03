88549e9a82293797d49c5e85d1cddf5ee8c7e815
Minhyun Cho <simh3077@gmail.com>
2022-06-03 22:12:28 +0900

    delete: NavigationView

--- AjouSelves_iOS_App/AjouSelves_iOS_App/View/baseView/settingView.swift
+++ AjouSelves_iOS_App/AjouSelves_iOS_App/View/baseView/settingView.swift
@@ -12,21 +12,19 @@ var ver: String = "v0.0.7"
 
 struct settingView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                Button(action: {
-                    print("\(UserDefaults.standard.string(forKey: "userToken"))")
+        VStack {
+            Button(action: {
+                print("\(UserDefaults.standard.string(forKey: "userToken"))")
+            }, label: {
+                Image(systemName: "bell")
+            })
+            NavigationLink(
+                destination: {
+                    Text("약관")
                 }, label: {
-                    Image(systemName: "bell")
+                    Text("약관")
                 })
-                NavigationLink(
-                    destination: {
-                        Text("약관")
-                    }, label: {
-                        Text("약관")
-                    })
-                Text("버전: \(ver)")
-            }
+            Text("버전: \(ver)")
         }
     }
 }
